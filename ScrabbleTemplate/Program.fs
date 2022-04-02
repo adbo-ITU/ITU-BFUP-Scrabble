2a6eb875991fd9cceed493761ca736aff70e2138
Adrian Borup <avborup@gmail.com>
2022-04-02 16:18:11 +0200

    Add player

--- ScrabbleTemplate/Program.fs
+++ ScrabbleTemplate/Program.fs
@@ -12,10 +12,10 @@ let readLines filePath = System.IO.File.ReadLines(filePath)
 
 let spawnMultiples name dict bot =
     let rec aux =
-        function 
+        function
         | 0 -> []
-        | x -> (sprintf "%s%d" name x, dict, bot)::aux (x - 1)
-   
+        | x -> (sprintf "%s%d" name x, dict, bot) :: aux (x - 1)
+
     aux >> List.rev
 
 [<EntryPoint>]
@@ -27,42 +27,44 @@ let main argv =
     System.Console.Clear()
 
 
-    let board        = ScrabbleUtil.StandardBoard.standardBoard ()
-//    let board      = ScrabbleUtil.InfiniteBoard.infiniteBoard ()
+    let board =
+        ScrabbleUtil.StandardBoard.standardBoard ()
+    //    let board      = ScrabbleUtil.InfiniteBoard.infiniteBoard ()
 
-//    let board      = ScrabbleUtil.RandomBoard.randomBoard ()
+    //    let board      = ScrabbleUtil.RandomBoard.randomBoard ()
 //    let board      = ScrabbleUtil.RandomBoard.randomBoardSeed (Some 42)
 //    let board      = ScrabbleUtil.InfiniteRandomBoard.infiniteRandomBoard ()
 //    let board      = ScrabbleUtil.InfiniteRandomBoard.infiniteRandomBoardSeed (Some 42)
 
-//    let board      = ScrabbleUtil.HoleBoard.holeBoard ()
+    //    let board      = ScrabbleUtil.HoleBoard.holeBoard ()
 //    let board      = ScrabbleUtil.InfiniteHoleBoard.infiniteHoleBoard ()
 
-    let words     = readLines "../../../Dictionaries/English.txt"
+    let words =
+        readLines "../../../Dictionaries/English.txt"
 
-    let handSize   = 7u
-    let timeout    = None
-    let tiles      = ScrabbleUtil.English.tiles 1u
-    let seed       = None
-    let port       = 13001
+    let handSize = 7u
+    let timeout = None
+    let tiles = ScrabbleUtil.English.tiles 1u
+    let seed = None
+    let port = 13001
 
     let dictAPI =
         // Uncomment if you have implemented a dictionary. last element None if you have not implemented a GADDAG
-        // Some (Dictionary.empty, Dictionary.insert, Dictionary.step, Some Dictionary.reverse) 
+        // Some (Dictionary.empty, Dictionary.insert, Dictionary.step, Some Dictionary.reverse)
         None
 
-    // Uncomment this line to call your client
-    // let players    = [("Your name here", YourClientName.Scrabble.startGame)]
     let (dictionary, time) =
         time (fun () -> ScrabbleUtil.Dictionary.mkDict words dictAPI)
 
-    let players = spawnMultiples "OxyphenButazone" dictionary Oxyphenbutazone.Scrabble.startGame 4
+    let players =
+        [ ("The Grafted", dictionary, TheGrafted.Scrabble.startGame) ]
 
+    // let players =
+    //     spawnMultiples "OxyphenButazone" dictionary Oxyphenbutazone.Scrabble.startGame 4
 
-    do ScrabbleServer.Comm.startGame 
-          board dictionary handSize timeout tiles seed port players
-    
+    do ScrabbleServer.Comm.startGame board dictionary handSize timeout tiles seed port players
+
     ScrabbleUtil.DebugPrint.forcePrint ("Server has terminated. Press Enter to exit program.\n")
-    System.Console.ReadLine () |> ignore
+    System.Console.ReadLine() |> ignore
 
     0
