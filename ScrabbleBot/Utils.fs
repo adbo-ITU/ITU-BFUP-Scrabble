f1563b7938da2f49b6063e66d4178ddf91cefd58
Joachim Borup <aljb@itu.dk>
2022-05-04 10:21:21 +0200

    Add utility coordsBetween function

--- ScrabbleBot/Utils.fs
+++ ScrabbleBot/Utils.fs
@@ -16,6 +16,20 @@ let rotate (step: uint32) input =
 
 let addCoords (a: int * int) (b: int * int) = (fst a + fst b, snd a + snd b)
 
+let coordsBetween a b =
+    // We know that either the horizontal or vertical distance is 0
+    let dist = abs (fst b - fst a + snd b - snd a) + 1
+
+    Seq.init (dist) (fun n ->
+        addCoords
+            a
+            (match a with
+             | (x, _) when x < fst b -> (n, 0)
+             | (x, _) when x > fst b -> (0, n)
+             | (_, y) when y < snd b -> (-n, 0)
+             | (_, y) when y > snd b -> (0, -n)
+             | _ -> failwith "coordinates not on the same axis"))
+
 let flatMap f opt =
     match opt with
     | Some x -> f x
